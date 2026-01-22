ba84e97d5e1b59d07fee47d252877f108fc1385b
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2026-01-22 17:52:47 +0900

    Update 01_read_raw.ino

--- examples/01_read_raw/01_read_raw.ino
+++ examples/01_read_raw/01_read_raw.ino
@@ -2,7 +2,6 @@
 
 // Sensor pins (맨 왼쪽 라인 검출 센서가 연결된 핀(4번)부터 입력)
 const uint8_t sensorPins[] = { 4, A3, 11, A0, A2, 5 };
-uint16_t sensors_raw_value[6];          // 6개 각각의 라인 검출 센서 값을 저장하기 위한 변수
 RiaLineTracerR4 tracer(sensorPins, 6);  // emitterPin default=2
 
 void setup() {
@@ -11,11 +10,12 @@ void setup() {
 }
 
 void loop() {
+  uint16_t sensors_raw_value[tracer.sensorCount()];          // 6개 각각의 라인 검출 센서 값을 저장하기 위한 변수
   tracer.readRaw(sensors_raw_value);    // 라인 검출 센서 6개로부터 Raw 값 읽기(us)
   
-  for (int i = 0; i < 6; i++) {
-    Serial.print(sensors_raw_value[i]);
-    Serial.print('\t');
+  for (int i = 0; i < tracer.sensorCount(); i++) {
+    Serial.print(raw[i]);
+    if (i < tracer.sensorCount() - 1) Serial.print('\t');
   }
   Serial.println();
   delay(100);
